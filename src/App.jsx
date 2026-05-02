a354ecaf19ce0645f7ae1e78609ac918bc03c9d3
Beharudin Mohammed <89333678+Beharudin@users.noreply.github.com>
2026-05-02 11:15:45 +0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header";
+import SmoothScroll from "./SmoothScroll";
+
+const App = () => {
+  return (
+    <>
+      <SmoothScroll>
+        <Header />
+        {/* <Hero /> */}
+        {/* <Taste /> */}
+        {/* <Quality /> */}
+        {/* <Parallax /> */}
+        {/* <Footer /> */}
+      </SmoothScroll>
+    </>
+  );
+};
+
+export default App;
